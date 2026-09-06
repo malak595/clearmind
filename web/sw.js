60d55046108cac2667d922a25f21271ba02769b9
malak595 <malakbendr@gmail.com>
2026-09-06 15:22:17 +0100

    Add overthinking support

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'clearmind-shell-v4';
+const CACHE_NAME = 'clearmind-shell-v5';
 const APP_SHELL = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', (event) => {
